43497300893f2d87819705c45f02a7b64bd36851
waweraty <waweraty@users.noreply.github.com>
2020-10-30 09:25:30 -0600

    Función KNN
    Gerardo Villegas

--- 1erAvance.py
+++ 1erAvance.py
@@ -13,6 +13,36 @@ from sklearn.decomposition import FactorAnalysis
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.neighbors import NeighborhoodComponentsAnalysis
 
+def knn(trainingSet, inst, k):
+    dists = {}
+    leng = inst.shape[1]
+    
+    #Calculo de la distancia euclideana entre cada fila de entrenamiento y de prueba
+    for i in range(len(trainingSet)):
+        dist = dE(inst, trainingSet.iloc[i], leng)
+        dists[i] = dist[0]
+
+    # Ordenando de menor a mayor en cuanto a distancia
+    ordenDist = sorted(dists.items(), key=operator.itemgetter(1))
+    neighbors = []
+    
+    # Extraemos los k vecinos más cercanos
+    for i in range(k):
+        neighbors.append(ordenDist[i][0])
+    classVotes = {}
+    
+    # Calculando la clase que más se repite en los vecinos
+    for i in range(len(neighbors)):
+        resp = trainingSet.iloc[neighbors[i]][len(trainingSet.columns)-1]
+        
+        if resp in classVotes:
+            classVotes[resp] += 1
+        else:
+            classVotes[resp] = 1
+
+    ordenVotos = sorted(classVotes.items(), key=operator.itemgetter(1), reverse=True)
+    return(ordenVotos[0][0], neighbors)
+
 sp=pd.read_csv('/Users/diegovelazquez/Downloads/iris.csv')
 sp['Tipo_Flor']=sp['Tipo_Flor'].replace(['Iris-versicolor','Iris-virginica','Iris-setosa'],[0,1,2])
 data=sp.values
