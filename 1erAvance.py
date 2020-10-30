93cb3afd9df5a771ad0d5edd0bcc847ec0eb74d6
isa-cr <a01066999@itesm.mx>
2020-10-29 20:03:27 -0600

    FactorA y Linear D
    Isabel

--- 1erAvance.py
+++ 1erAvance.py
@@ -19,6 +19,18 @@ data=sp.values
 X= data[:,0:-1]
 y=data[:,-1]
 
+emb= FactorAnalysis(n_components=2)
+X1t =emb.fit_transform(X,y)
+plt.scatter(X1t[:,0],X1t[:,-1],c=y)
+plt.title('Iris dataset FactorAnalysis')
+plt.show()
+
+emb= LinearDiscriminantAnalysis(n_components=2)
+X2t =emb.fit_transform(X,y)
+plt.scatter(X2t[:,0],X2t[:,-1],c=y)
+plt.title('Iris dataset LinearDiscriminant')
+plt.show()
+
 emb=NeighborhoodComponentsAnalysis(n_components=2)
 X3t =emb.fit_transform(X,y)
 plt.scatter(X3t[:,0],X3t[:,-1],c=y)   
